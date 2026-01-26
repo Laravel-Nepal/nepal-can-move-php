4175c97e2167ddde3d3d73fc1b541f588c5cb414
achyutkneupane <achyutkneupane@gmail.com>
2026-01-26 20:42:43 +0545

    test: for COD transfer ticket creation and exception handling

--- tests/Feature/TicketTest.php
+++ tests/Feature/TicketTest.php
@@ -8,7 +8,7 @@ it('throws an exception on wrong ticket type', function () {
     $this->expectException(\AchyutN\NCM\Exceptions\NCMException::class);
 
     $ncm->createSupportTicket('InvalidType', 'This is error ticket.');
-})->only();
+});
 
 it('can create a support ticket', function () {
     $ncm = ncm();
@@ -16,4 +16,28 @@ it('can create a support ticket', function () {
     $result = $ncm->createSupportTicket('General', 'This is a test support ticket created from SDK.');
 
     expect($result)->toBeTrue();
+});
+
+describe('COD transfer ticket', function () {
+    $ncm = ncm();
+
+    $ticketId = $ncm->createCODTransferTicket(
+        bankName: 'Test Bank',
+        accountHolderName: 'Achyut Neupane',
+        accountNumber: '1234567890'
+    );
+
+    it('can create a COD transfer ticket', function () use ($ticketId) {
+        expect($ticketId)->toBeInt()->toBeGreaterThan(0);
+    });
+
+    it('throws an exception on second COD ticket', function () use ($ncm) {
+        $this->expectException(\AchyutN\NCM\Exceptions\NCMException::class);
+
+        $ncm->createCODTransferTicket(
+            bankName: 'Test Bank',
+            accountHolderName: 'Achyut Neupane',
+            accountNumber: '1234567890'
+        );
+    });
 });
